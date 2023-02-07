02adbebaddfd8827fd001fa272c55d8b83378f44
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-02-07 12:49:21 -0300

    Adição atalhos de outras redes sociais

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,8 +6,13 @@ export function Header() {
         <header>
             <h1>Leandro Lincoln</h1>
             <nav>
+            <SocialLink url="https://linkedin.com/in/leandrolincoln"
+            icon="fa-brands fa-linkedin" />
             <SocialLink url="https://github.com/leandrolinkolnr"
             icon="fa-brands fa-github" />
+            <SocialLink url="https://instagram.com/leandrolinkoln/"
+            icon="fa-brands fa-instagram" />
+
             </nav>
         </header>
     )
