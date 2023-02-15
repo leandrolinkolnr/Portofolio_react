b727bf85811bc48e983c346b7980cb8ac3ca3374
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-02-15 15:37:53 -0300

    Update description

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -4,7 +4,7 @@ export function Description() {
     return(
         <article className="text">
             <h2>👨🏽‍💻 Sobre mim</h2>
-            <p>, Olá, me chamo Leandro Lincoln, 27 anos tal, tal e tal, Olá, me chamo Leandro Lincoln, 27 anos tal, tal e tal</p>
+            <p>, Olá, me chamo Leandro Lincoln, tenho 27 anos e sou de Campina Grande, Paraiba. Atuo como desenvolvedor web e tenho graduação em Ciência da Computação(UEPB). Apaixonado pelo desenvolvimento de software e Data Science. Em busca de oportunidades para o meu desenvolvimento profissional. </p>
         </article>
     )
 }
